23120afa2879ac8a4a8420546480c34e606d1c00
kalp121212 <78316352+kalp121212@users.noreply.github.com>
2021-08-03 21:45:38 +0530

    Add files via upload

--- test_model.py
+++ test_model.py
@@ -1,14 +1,27 @@
+from sudoku import Detector
 from model import Trainer
 
-def testModel():
-	t = Trainer()
-	t.load_data()
+def testSolver():
+	d = Detector()
 
-	try:
-		t.load_model()
-	except:
-		t.train()
-	
-	acc = t.test()
+	with open('assets/sudokus/sudoku1.txt', 'r') as file:
+		answer = [list(map(int, line.strip('\n'))) for line in file.readlines()]
 
-	assert acc > 0.9, "Accuracy not high enough"
+	## Add correction array
+	result = d.run(path='assets/sudokus/sudoku1.jpg', corrections=[[0,5,4],[5,4,1],[8,5,1]])
+
+	for i in range(9):
+		for j in range(9):
+			assert result[i][j] == answer[i][j]
+
+	with open('assets/sudokus/sudoku2.txt', 'r') as file:
+		answer = [list(map(int, line.strip('\n'))) for line in file.readlines()]
+
+	d = Detector()
+
+	# Add correction array
+	result = d.run(path='assets/sudokus/sudoku2.jpg', corrections=[[2,4,9],[5,7,9]])
+
+	for i in range(9):
+		for j in range(9):
+			assert result[i][j] == answer[i][j]
